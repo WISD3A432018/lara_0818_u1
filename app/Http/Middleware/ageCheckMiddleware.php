4b7d19a70bfb8a0c7b8f202bea98d5dbd002e7d9
img21326 <img21326@gmail.com>
2018-01-05 19:50:58 +0800

    complete checkage

--- app/Http/Middleware/ageCheckMiddleware.php
+++ app/Http/Middleware/ageCheckMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class ageCheckMiddleware
 {
@@ -15,6 +16,11 @@ class ageCheckMiddleware
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        if (Auth::check()) {
+            if(Auth::user()->age > 18){
+                return $next($request);
+            }
+        }
+        return redirect('/home');
     }
 }
